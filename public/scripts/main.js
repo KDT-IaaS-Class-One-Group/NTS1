f1287a894fe9f87e60f5d61c70fb8649219e46da
최은철 <wndqhrgkdlel@gmail.com>
2023-11-17 14:16:43 +0900

    [docs] css 기능 구현, 위치나 내용은 수정해야 됨

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,44 +1,8 @@
-const header = document.getElementById('header');
-
-// Create hamburger-menu div
-const hamburgerMenu = document.createElement('div');
-hamburgerMenu.style.cursor = 'pointer';
-hamburgerMenu.style.display = 'flex';
-hamburgerMenu.style.flexDirection = 'column';
-hamburgerMenu.id = 'hamburger-menu';
-
-// Create three bars for the hamburger icon
-for (let i = 0; i < 3; i++) {
-    const bar = document.createElement('div');
-    bar.style.backgroundColor = '#fff';
-    bar.style.height = '3px';
-    bar.style.width = '25px';
-    bar.style.margin = '5px 0';
-    bar.style.transition = '0.4s';
-    hamburgerMenu.appendChild(bar);
-}
-
-// Add hamburger-menu div to the header
-header.appendChild(hamburgerMenu);
-
-// Toggle function remains the same
-let isOpen = false;
-
-function toggleMenu() {
-    const bars = document.querySelectorAll('#hamburger-menu > div');
-
-    if (isOpen) {
-        bars.forEach(bar => {
-            bar.style.transform = 'rotate(0deg)';
-        });
+function toggleNavbar() {
+    var navbar = document.getElementById("myNavbar");
+    if (navbar.style.width === "250px") {
+        navbar.style.width = "0";
     } else {
-        bars.forEach((bar, index) => {
-            const rotateDegree = (index + 1) * 45;
-            bar.style.transform = `rotate(${rotateDegree}deg)`;
-        });
+        navbar.style.width = "250px";
     }
-
-    isOpen = !isOpen;
 }
-
-hamburgerMenu.addEventListener('click', toggleMenu);
