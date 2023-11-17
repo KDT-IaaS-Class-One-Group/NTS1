003de6fd0f5111417dfb059015ee626a6095bc10
최은철 <wndqhrgkdlel@gmail.com>
2023-11-17 14:31:52 +0900

    [docs] 일부 태그들 동적 생성

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,3 +6,22 @@ function teamToggle() {
         navbar.style.width = "250px";
     }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    // DOM이 로드된 후 실행될 코드
+
+    // #main 요소에 추가할 새로운 div 요소 생성
+    let newContent = document.createElement("div");
+    newContent.innerHTML = `<span class="menuIcon" onclick="teamToggle()">&#9776;</span>
+    <div class="navbar" id="myNavbar">
+      <span class="menuIcon" onclick="teamToggle()">&#9776;</span>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#services">Services</a>
+      <a href="#contact">Contact</a>
+  </div>`;
+
+    // #main 요소에 새로운 div 요소 추가
+    let mainElement = document.getElementById("header");
+    mainElement.appendChild(newContent);
+});
