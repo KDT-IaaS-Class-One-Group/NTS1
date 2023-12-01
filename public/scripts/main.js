4b41be03236fe0ffb64d1a61bdd99e7e3a4eeac9
ohseungmin <ohmin2886@gmail.com>
2023-12-01 18:04:11 +0900

    import로 모듈화한 슬라이드 기능을 main.js에 불러오기

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,55 +1,21 @@
-// 상태 관리 변수이며 현재 슬라이드의 번호를 추적하는 역할
+import { initializeSlideShow, showSlide, getPrevSlide, getNextSlide } from './slide.js';
+
+// 슬라이드 쇼 초기화
+initializeSlideShow();
+
+// 예시로 슬라이드 조작
+const slides = document.querySelectorAll(".slide");
 let currentSlide = 1;
 
-function showSlide(number) {
-  const slides = document.querySelectorAll(".slide");
-  if (number > slides.length) {
-    currentSlide = 1;
-  }
-  if (number < 1) {
-    currentSlide = slides.length;
-  }
-
-  slides.forEach((slide, index) => {
-    // 현재 슬라이드는 활성화되어 나타나도록 설정
-    if (index + 1 === currentSlide) {
-      slide.classList.add("active");
-      slide.classList.remove("previous", "next");
-    } else if (index + 1 < currentSlide) {
-      // 이전 슬라이드는 왼쪽으로 이동하여 사라지도록 설정
-      slide.classList.remove("active", "next");
-      slide.classList.add("previous");
-    } else {
-      // 다음 슬라이드는 오른쪽으로 이동하여 나타나도록 설정
-      slide.classList.remove("active", "previous");
-      slide.classList.add("next");
-    }
-  });
-}
-
-
-// 이전 슬라이드로 이동하는 함수
-// -= 1 = 현 슬라이드가 2번일 경우 1번 슬라이드로 갈 수 있게 1씩 마이너스하는 역할
-function prevSlide() {
-  showSlide((currentSlide -= 1));
-}
-
-// 다음 슬라이드로 이동하는 함수
-// += = 현 슬라이드에서 1씩 추가되는 1 슬라이드일 경우 2 슬라이드 이동하는 함수
-function nextSlide() {
-  showSlide((currentSlide += 1));
-}
-
-// 버튼에 대한 이벤트 처리를 추가합니다.
 document.querySelector(".prev").addEventListener("click", () => {
-  prevSlide();
+  currentSlide = getPrevSlide(slides.length, currentSlide);
+  showSlide(slides, currentSlide);
 });
 
 document.querySelector(".next").addEventListener("click", () => {
-  nextSlide();
+  currentSlide = getNextSlide(slides.length, currentSlide);
+  showSlide(slides, currentSlide);
 });
-
-showSlide(currentSlide);
 
 function teamToggle() {
   let navbar = document.getElementById("myNavbar");
