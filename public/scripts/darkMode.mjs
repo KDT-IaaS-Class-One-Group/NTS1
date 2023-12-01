1899300d66bb05fb58c6eb745dffa324cd1aaf07
최은철 <wndqhrgkdlel@gmail.com>
2023-12-01 18:01:48 +0900

    코드 정렬

--- public/scripts/darkMode.mjs
+++ public/scripts/darkMode.mjs
@@ -1,12 +1,12 @@
 export function darkMode() {
-    const body = document.body;
-    const titleBox = document.getElementById("titleBox");
-    const toggleButton = document.getElementById("darktoggleButton");
-  
-    toggleButton.addEventListener("click", function () {
-      body.classList.toggle("dark-mode");
-      titleBox.style.backgroundColor = body.classList.contains("dark-mode")
-        ? "#394734"
-        : "#568444";
-    });
-  }
+  const body = document.body;
+  const titleBox = document.getElementById("titleBox");
+  const toggleButton = document.getElementById("darktoggleButton");
+
+  toggleButton.addEventListener("click", function () {
+    body.classList.toggle("dark-mode");
+    titleBox.style.backgroundColor = body.classList.contains("dark-mode")
+      ? "#394734"
+      : "#568444";
+  });
+}
